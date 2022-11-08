cab4287f7f238b405ee309c9e202c5ded5bf5060
rjmchi <robert@rjmprogramming.com>
2022-11-08 17:34:28 -0600

    adds api for skills

---
+++ app/Http/Controllers/SkillController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSkillRequest;
+use App\Http\Resources\SkillResource;
+use App\Models\Skill;
+use Illuminate\Http\Request;
+
+class SkillController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json("Skill Index");
+    }
+
+      /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSkillRequest $request)
+    {
+        Skill::create($request->validated());
+        return response()->json("Skill Created");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Skill  $skill
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Skill $skill)
+    {
+        return new SkillResource($skill);
+    }
+
+     /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Skill  $skill
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreSkillRequest $request, Skill $skill)
+    {
+        $skill->update($request->validated());
+        return response()->json("Skill Updated");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Skill  $skill
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Skill $skill)
+    {
+        $skill->delete();
+        return response()->json("Skill Deleted");
+    }
+}
---
+++ app/Http/Requests/StoreSkillRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreSkillRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'min:3', 'max:20'],
+            // 'slug' => ['required','unique:skills,slug,' . $this->skill->id]
+            // 'slug' => ['required','unique:skills,slug']
+            'slug' => ['required',Rule::unique('skills')->ignore($this->skill)]
+        ];
+    }
+}
